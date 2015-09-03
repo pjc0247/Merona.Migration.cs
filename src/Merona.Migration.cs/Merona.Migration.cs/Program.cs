eb2dd4d66ebae00484f458503ad42d111a4aed09
pjc02478 <pjc0247@gmail.com>
2015-09-03 18:05:19 +0900

    initia

---
+++ src/Merona.Migration.cs/Merona.Migration.cs/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Merona.Migration.cs
+{
+    /// <summary>
+    /// 이전 모델들을 담고 있는 클래스임을 나타내는 속성입니다.
+    /// </summary>
+    public class OldModels : Attribute
+    {
+    }
+    /// <summary>
+    /// 마이그레이션할 모델들을 담고 있는 클래스임을 나타내는 속성입니다.
+    /// </summary>
+    public class NewModels : Attribute
+    {
+    }
+
+    public class Model
+    {
+        /// <summary>
+        /// 해당 필드에 인덱스를 생성함을 나타내는 속성입니다.
+        /// </summary>
+        protected class Index : Attribute
+        {
+        }
+    }
+
+
+    class Program
+    {
+        /// <summary>
+        /// 두 모델 간의 변경 사항을 찾는다.
+        /// </summary>
+        /// <param name="old">이전 버전 모델</param>
+        /// <param name="to">마이그레이션할 대상 모델</param>
+        static void FindDiffProps(Type old, Type to)
+        {
+            var oldProps = old.GetProperties();
+            var toProps = to.GetProperties();
+
+            var mutual = from toProp in toProps
+                         join oldProp in oldProps on toProp.Name equals oldProp.Name
+                         select toProp;
+
+            var added = from toProp in toProps
+                        where
+                          !(from oldProp in oldProps
+                            select oldProp.Name).Contains(toProp.Name)
+                        select toProp;
+            var removed = from oldProp in oldProps
+                          where
+                            !(from toProp in toProps
+                              select toProp.Name).Contains(oldProp.Name)
+                          select oldProp;
+
+            foreach (var prop in mutual)
+                Console.WriteLine(prop.Name);
+            Console.WriteLine();
+
+            foreach (var prop in added)
+                Console.WriteLine(prop.Name);
+            Console.WriteLine();
+
+            foreach (var prop in removed)
+                Console.WriteLine(prop.Name);
+        }
+
+        /// <summary>
+        /// 두 모델 집합 간의 변경 사항을 찾는다.
+        /// </summary>
+        /// <param name="old">이전 버전의 모델 집합</param>
+        /// <param name="to">마이그레이션할 대상 모델 집합</param>
+        static void FindDiffModels(Type old, Type to)
+        {
+            var oldModels = old.GetNestedTypes();
+            var toModels = to.GetNestedTypes();
+
+            var mutual = from toModel in toModels
+                         join oldModel in oldModels on toModel.Name equals oldModel.Name
+                         select toModel;
+
+            var added = from toModel in toModels
+                        where
+                          !(from oldModel in oldModels
+                            select oldModel.Name).Contains(toModel.Name)
+                        select toModel;
+            var removed = from oldModel in oldModels
+                          where
+                            !(from toModel in toModels
+                              select toModel.Name).Contains(oldModel.Name)
+                          select oldModel;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
